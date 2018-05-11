d33860bf52e617898a7cceb26b1387ef4f840866
RonanMacF <macfhlar@tcd.ie>
2018-05-11 21:47:26 +0100

    Mongoose User Schema complete

---
+++ routes/models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  Password: {
+    type: String,
+    required: true
+  },
+  Avatar: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = User = mongoose.model("users", UserSchema);
