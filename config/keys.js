23bdeb39a6560d9cb7373c00b671c0e2d2f7a304
RonanMacF <macfhlar@tcd.ie>
2018-05-11 17:33:38 +0100

    Connected to mLab mongoDB

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://ronan:rossmore@ds119070.mlab.com:19070/develeire"
+};
--- server.js
+++ server.js
@@ -1,6 +1,15 @@
+// Imports
 const express = require("express");
+const mongoose = require("mongoose");
 
+// Initialise Express Object
 const app = express();
+const db = require("./config/keys").mongoURI;
+
+mongoose
+  .connect(db)
+  .then(() => console.log("connected"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello"));
 
