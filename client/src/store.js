3d02533b878bdc59d97a23935d4720eefe9b9318
RonanMacF <macfhlar@tcd.ie>
2018-05-15 19:09:21 +0100

    Began Redux Implementation, Store Created & Middleware Applied

---
+++ client/src/store.js
@@ -0,0 +1,20 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers/index";
+
+const middleware = [thunk];
+const initialState = {};
+
+const composeEnhancer =
+  process.env.NODE_ENV !== "production" &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        name: "App",
+        actionBlacklist: ["REDUX_STORAGE_SAVE"]
+      })
+    : compose;
+
+const enhancer = composeEnhancer(applyMiddleware(...middleware));
+const store = createStore(rootReducer, initialState, enhancer);
+
+export default store;
