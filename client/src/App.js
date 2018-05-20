d8b00f416c8ad47396d73aea2b31743d927d73ea
RonanMacF <macfhlar@tcd.ie>
2018-05-20 18:00:33 +0100

    Dashboard/Profile Reducer and action creater complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,48 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
+//import { clearCurrentProfile } from "./actions/profileActions";
+
 import { Provider } from "react-redux";
+import store from "./store";
+
+//import PrivateRoute from "./components/common/PrivateRoute";
 
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
-import store from "./store";
+//import Dashboard from "./components/dashboard/Dashboard";
+//import CreateProfile from "./components/create-profile/CreateProfile";
+
 import "./App.css";
+
+// Check for token
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  console.log(currentTime);
+  console.log(decoded.exp);
+
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Clear current Profile
+    //store.dispatch(clearCurrentProfile());
+    // Redirect to login
+    window.location.href = "/login";
+  }
+}
 
 class App extends Component {
   render() {
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 import errorReducer from "./errorReducer";
+import profileReducer from "./profileReducer";
 
 // Root Reducer
 export default combineReducers({
   auth: authReducer, // Contains userData and whether are authenticated or not
-  errors: errorReducer // Contains all the errors for form validation
+  errors: errorReducer, // Contains all the errors for form validation
+  profile: profileReducer
 });
