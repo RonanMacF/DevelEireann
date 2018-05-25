02b1c88a18f4bb1551b69b004136a32792b65942
RonanMacF <macfhlar@tcd.ie>
2018-05-25 11:53:43 +0100

    Minor Tweaks

--- client/src/components/common/SelectListGroup.js
+++ client/src/components/common/SelectListGroup.js
@@ -2,50 +2,37 @@ import React from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 
-const TextFieldGroup = ({
-  name,
-  placeholder,
-  value,
-  label,
-  error,
-  info,
-  type,
-  onChange,
-  disabled
-}) => {
+const SelectListGroup = ({ name, value, error, info, onChange, options }) => {
+  const selectOptions = options.map(option => (
+    <option key={option.label} value={option.value}>
+      {option.label}
+    </option>
+  ));
   return (
     <div className="form-group">
-      <input
-        type={type}
+      <select
         className={classnames('form-control form-control-lg', {
-          'is-invalid': error // If there is an error display the is-invalid class
+          'is-invalid': error
         })}
-        placeholder={placeholder}
         name={name}
         value={value}
         onChange={onChange}
-        disabled={disabled}
-      />
+      >
+        {selectOptions}
+      </select>
       {info && <small className="form-text text-muted">{info}</small>}
       {error && <div className="invalid-feedback">{error}</div>}
     </div>
   );
 };
 
-TextFieldGroup.propTypes = {
+SelectListGroup.propTypes = {
   name: PropTypes.string.isRequired,
-  placeholder: PropTypes.string,
   value: PropTypes.string.isRequired,
   info: PropTypes.string,
   error: PropTypes.string,
-  type: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
-  disabled: PropTypes.string
+  options: PropTypes.array.isRequired
 };
 
-// defaults values to an empty string ""
-TextFieldGroup.defaultProps = {
-  type: 'text'
-};
-
-export default TextFieldGroup;
+export default SelectListGroup;
