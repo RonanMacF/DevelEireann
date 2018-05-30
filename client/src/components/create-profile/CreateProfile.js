0e19953fbaaa58070f0fcdbb68b8a4d5f656e0db
RonanMacF <macfhlar@tcd.ie>
2018-05-30 22:29:20 +0100

    update

---
+++ client/src/components/create-profile/CreateProfile.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class CreateProfile extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+CreateProfile.propTypes = {
+  profile: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  profile: state.profile,
+  errors: state.errors
+});
+
+export default connect(mapStateToProps)(CreateProfile);
